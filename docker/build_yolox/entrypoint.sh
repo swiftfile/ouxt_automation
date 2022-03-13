deb5d89df29dad832fa51760168d4873816c0b13
MasayaKataoka <ms.kataoka@gmail.com>
2022-03-14 01:19:26 +0900

    update entrypoint
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- docker/build_yolox/entrypoint.sh
+++ docker/build_yolox/entrypoint.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.2/targets/aarch64-linux/lib
 cmake ../ -DCMAKE_LIBRARY_PATH=/usr/local/cuda-10.2/targets/aarch64-linux/lib -DCUDA_CUDART_LIBRARY=/usr/local/cuda-10.2/targets/aarch64-linux/lib -DCMAKE_CXX_FLAGS='-Wl,--allow-shlib-undefined'
-# make -j12
-# while :; do sleep 10; done
+make -j12
+while :; do sleep 10; done
