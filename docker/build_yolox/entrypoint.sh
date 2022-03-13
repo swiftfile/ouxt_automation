5bbc5b4c247cd90ffde0db56f0f0c80cb345eeec
MasayaKataoka <ms.kataoka@gmail.com>
2022-03-14 01:18:41 +0900

    add build yolox image
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

---
+++ docker/build_yolox/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd demo/TensorRT/cpp
+mkdir build
+cd build
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.2/targets/aarch64-linux/lib
+cmake ../ -DCMAKE_LIBRARY_PATH=/usr/local/cuda-10.2/targets/aarch64-linux/lib -DCUDA_CUDART_LIBRARY=/usr/local/cuda-10.2/targets/aarch64-linux/lib -DCMAKE_CXX_FLAGS='-Wl,--allow-shlib-undefined'
+# make -j12
+# while :; do sleep 10; done
